ffb17b05521e5cdea9f4f1cce76f3e29dca297c4
JakDab <bloomo.private@gmail.com>
2022-11-10 13:13:25 +0000

    Update About.jsx

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -12,22 +12,21 @@ const About = () => {
         transition={{ duration: 0.5 }}
         className="app__header-info"
       >
+        
+        <motion.div 
+          whileInView={{ opacity: [0, 1] }}
+          transition={{ duration: 0.5, delayChildren: 0.5 }}
+          className="app__header-img"
+        >
+          <img src={images.profile} alt="Hero Logo Image"/>
+        </motion.div> 
+        
         <div className="app__header-content">
-          <h1 className="h1-text">discover new worlds</h1>
-          <p1 className="p-text">Ruwamo is a stand-alone game development studio which offers games on consoles and mobile devices</p1>
-            
-          {/* <div className="tag-cmp app__flex">
-            CTA
-          </div> */}
+          <h1 className="h1-text">About Us</h1>
+          <p1 className="p-text">Ruvamo is a newly founded game development studio located in Cracow, Poland. Our aim at Ruvamo is to make the most enjoybale games for the players all around the globe, who wish exape the Earth’s barriers for a period of time. </p1>
+          <h1 className="h1-text">Contact</h1>
+          <p1 className="p-text">Please feel free to contact us: contact@ruwamogames.com</p1>
         </div>
-      </motion.div>
-
-      <motion.div 
-        whileInView={{ opacity: [0, 1] }}
-        transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className="app__header-img"
-      >
-        <img src={images.profile} alt="Hero Logo Image"/>
       </motion.div>
     </div>
   )
