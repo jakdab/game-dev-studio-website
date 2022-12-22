74812d656ed00961ca717385f44dec6201eb71a7
JakDab <bloomo.private@gmail.com>
2022-12-22 16:39:11 +0100

    Responsive Layout Update

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -5,19 +5,24 @@ import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div className="app__footer">
+    <div id="footer" className="app__footer">
       <div className="app__column">
-        <img src={images.logo2} alt='logo' />
+        <div className="app__logo">
+          <img src={images.logo2} alt='logo' />
+        </div>
       </div>
       <div className="app__column">
         <p1 className="p2-text">
-            We want to provide entertainment for whole family and that is why we aim at budget, high-level of polish titles for everybody!</p1>
+            We want to provide entertainment for whole 
+            family and that is why we aim at budget, high-
+            level of polish titles for everybody!
+        </p1>
       </div>
-      <div className="app__column">
-        <p1 className="p2-text">All rights reserved RUWAMO Games</p1>
+      <div className="app__column app__column-meta">
+        <p1 className="p2-text">RUWAMO Games © 2022 </p1>
         <div className="app__row">
-          <img className="icon" src={images.twitter} alt='twitter link'/>
-          <img className="icon" src={images.youtube} alt='youtube link'/>
+          <img className="icon" href="https://twitter.com/ruwamogames" src={images.twitter} alt='twitter link'/>
+          <img className="icon" href="https://www.youtube.com/@ruwamogames6030" src={images.youtube} alt='youtube link'/>
         </div>
       </div>
     </div>
